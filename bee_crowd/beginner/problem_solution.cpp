f9d338f40077fe20c2e71b6fd4814b8f204fdb5e
EftekharAlam2 <hinsouvlmare@gmail.com>
2024-01-18 20:49:20 +0600

    3 problems solved

--- bee_crowd/beginner/problem_solution.cpp
+++ bee_crowd/beginner/problem_solution.cpp
@@ -95,9 +95,9 @@ using namespace std;
 int main() {
  
     double A,B,MEDIA;
-    cin>>A;
-    cin>>B;
-    MEDIA=((A*3.5)+(B*7.5))/(A+B);
+    cin>>setprecision(1)>>A;
+    cin>>setprecision(1)>>B;
+    MEDIA=((A*3.5)+(B*7.5))/(3.5+7.5);
     
     cout<<fixed<<setprecision(5);
     cout<<"MEDIA = "<<MEDIA<<endl;
@@ -105,3 +105,44 @@ int main() {
     return 0;
 }
 
+
+// Problem 07 - Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one decimal place.
+// The input file contains 3 values of floating points (double) with one digit after the decimal point.
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+    double A,B,C,MEDIA;
+
+    cin>>setprecision(1)>>A;
+    cin>>setprecision(1)>>B;
+    cin>>setprecision(1)>>C;
+    MEDIA=((A*2)+(B*3)+(C*5))/(2+3+5);
+
+    cout<<fixed<<setprecision(1);
+    cout<<"MEDIA = "<<MEDIA<<endl;
+    return 0;
+}
+
+
+// Problem 08 - Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+// The input file contains 4 integer values.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int A,B,C,D,DIFERENCA;
+    cin>>A;
+    cin>>B;
+    cin>>C;
+    cin>>D;
+    DIFERENCA = (A*B)-(C*D);
+
+    cout<<"DIFERENCA = "<<DIFERENCA<<endl;
+    return 0;
+}
