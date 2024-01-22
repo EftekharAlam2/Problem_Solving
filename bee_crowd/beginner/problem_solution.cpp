4c997c562a6e1c3b49e1f1a3c58e69ed94adb760
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-01-22 12:03:18 +0600

    37 beginners problems solved

--- bee_crowd/beginner/problem_solution.cpp
+++ bee_crowd/beginner/problem_solution.cpp
@@ -1,4 +1,4 @@
-// Problem 01 - Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+// Problem 1001 - Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
 // The input file will contain 2 integer numbers
 
 #include <iostream>
@@ -15,7 +15,7 @@ int main() {
     return 0;
 }
 
-// Problem 02 - The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+// Problem 1002 - The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
 // Calculate the area using the formula given in the problem description.
 // The input contains a value of floating point (double precision), that is the variable R.
 
@@ -36,7 +36,7 @@ int main() {
     return 0;
 }
 
-// Problem 03 - Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.
+// Problem 1003 - Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.
 // The input file contains 2 integer numbers.
 
 #include <iostream>
@@ -55,7 +55,7 @@ int main() {
     return 0;
 }
 
-// Problem 04 - Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
+// Problem 1004 - Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
 // The input file contains 2 integer numbers.
 
 #include <iostream>
@@ -74,7 +74,7 @@ int main() {
     return 0;
 }
 
-// Problem 05 - Read two floating points' values of double precision A and B, corresponding to two student's grades. After this, calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. Each grade can be from zero to ten, always with one digit after the decimal point. Don’t forget to print the end of line after the result, otherwise you will receive “Presentation Error”. Don’t forget the space before and after the equal sign.
+// Problem 1005 - Read two floating points' values of double precision A and B, corresponding to two student's grades. After this, calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. Each grade can be from zero to ten, always with one digit after the decimal point. Don’t forget to print the end of line after the result, otherwise you will receive “Presentation Error”. Don’t forget the space before and after the equal sign.
 // The input file contains 2 floating points' values with one digit after the decimal point.
 
 #include <iostream>
@@ -96,7 +96,7 @@ int main() {
 }
 
 
-// Problem 06 - Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one decimal place.
+// Problem 1006 - Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one decimal place.
 // The input file contains 3 values of floating points (double) with one digit after the decimal point.
 
 #include<iostream>
@@ -118,7 +118,7 @@ int main(){
 }
 
 
-// Problem 07 - Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+// Problem 1007 - Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
 // The input file contains 4 integer values.
 
 #include<iostream>
@@ -137,7 +137,7 @@ int main(){
     return 0;
 }
 
-// Problem 08 - Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
+// Problem 1008 - Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
 // Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
 // Don’t forget the space before and after the equal signal and after the U$.
 // The input file contains 2 integer numbers and 1 value of floating point, representing the number, worked hours amount and the amount the employee receives per worked hour.
@@ -165,7 +165,7 @@ int main(){
 }
 
 
-// Problem 09 - Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money). Considering that this seller receives 15% over all products sold, write the final salary (total) of this seller at the end of the month , with two decimal places.
+// Problem 1009 - Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money). Considering that this seller receives 15% over all products sold, write the final salary (total) of this seller at the end of the month , with two decimal places.
 // - Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
 // - Don’t forget the blank spaces.
 // The input file contains a text (employee's first name), and two double precision values, that are the seller's salary and the total value sold by him/her.
@@ -193,7 +193,7 @@ int main(){
 }
 
 
-// Problem 10 - In this problem, the task is to read a code of a product 1, the number of units of product 1, the price for one unit of product 1, the code of a product 2, the number of units of product 2 and the price for one unit of product 2. After this, calculate and show the amount to be paid.
+// Problem 1010 - In this problem, the task is to read a code of a product 1, the number of units of product 1, the price for one unit of product 1, the code of a product 2, the number of units of product 2 and the price for one unit of product 2. After this, calculate and show the amount to be paid.
 // The input file contains two lines of data. In each line there will be 3 values: two integers and a floating value with 2 digits after the decimal point.
 
 #include<iostream>
@@ -219,7 +219,7 @@ int main(){
 
 }
 
-// Problem 11 - Make a program that calculates and shows the volume of a sphere being provided the value of its radius (R) . The formula to calculate the volume is: (4/3) * pi * R3. Consider (assign) for pi the value 3.14159.
+// Problem 1011 - Make a program that calculates and shows the volume of a sphere being provided the value of its radius (R) . The formula to calculate the volume is: (4/3) * pi * R3. Consider (assign) for pi the value 3.14159.
 // Tip: Use (4/3.0) or (4.0/3) in your formula, because some languages (including C++) assume that the division's result between two integers is another integer. :)
 // The input contains a value of floating point (double precision).
 
@@ -241,7 +241,7 @@ int main(){
 
 }
 
-// Problem 12 - Make a program that reads three floating point values: A, B and C. Then, calculate and show:
+// Problem 1012 - Make a program that reads three floating point values: A, B and C. Then, calculate and show:
 // a) the area of the rectangled triangle that has base A and height C.
 // b) the area of the radius's circle C. (pi = 3.14159)
 // c) the area of the trapezium which has A and B by base, and C by height.
@@ -274,7 +274,7 @@ int main(){
     return 0;
 }
 
-// Problem 13 - Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
+// Problem 1013 - Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
 // ______
 // The input file contains 3 integer values.
 
@@ -301,7 +301,7 @@ int main(){
     return 0;
 }
 
-// Problem 14 - Calculate a car's average consumption being provided the total distance traveled (in Km) and the spent fuel total (in liters).
+// Problem 1014 - Calculate a car's average consumption being provided the total distance traveled (in Km) and the spent fuel total (in liters).
 // The input file contains two values: one integer value X representing the total distance (in Km) and the second one is a floating point number Y  representing the spent fuel total, with a digit after the decimal point.
 
 #include<iostream>
@@ -324,7 +324,7 @@ int main(){
     return 0;
 }
 
-// Problem 15 - Read the four values corresponding to the x and y axes of two points in the plane, p1 (x1, y1) and p2 (x2, y2) and calculate the distance between them, showing four decimal places after the comma, according to the formula:
+// Problem 1015 - Read the four values corresponding to the x and y axes of two points in the plane, p1 (x1, y1) and p2 (x2, y2) and calculate the distance between them, showing four decimal places after the comma, according to the formula:
 // __________
 // The input file contains two lines of data. The first one contains two double values: x1 y1 and the second one also contains two double values with one digit after the decimal point: x2 y2.
 
@@ -348,7 +348,7 @@ int main(){
     return 0;
 }
 
-// Problem 16 - Two cars (X and Y) leave in the same direction. The car X leaves with a constant speed of 60 km/h and the car Y leaves with a constant speed of 90 km / h.
+// Problem 1016 - Two cars (X and Y) leave in the same direction. The car X leaves with a constant speed of 60 km/h and the car Y leaves with a constant speed of 90 km / h.
 // In one hour (60 minutes) the car Y can get a distance of 30 kilometers from the X car, in other words, it can get away one kilometer for each 2 minutes.
 // Read the distance (in km) and calculate how long it takes (in minutes) for the car Y to take this distance in relation to the other car.
 // The input file contains 1 integer value.
@@ -368,7 +368,7 @@ int main(){
     return 0;
 }
 
-// Problem 17 - Little John wants to calculate and show the amount of spent fuel liters on a trip, using a car that does 12 Km/L. For this, he would like you to help him through a simple program. To perform the calculation, you have to read spent time (in hours) and the same average speed (km/h). In this way, you can get distance and then, calculate how many liters would be needed. Show the value with three decimal places after the point.
+// Problem 1017 - Little John wants to calculate and show the amount of spent fuel liters on a trip, using a car that does 12 Km/L. For this, he would like you to help him through a simple program. To perform the calculation, you have to read spent time (in hours) and the same average speed (km/h). In this way, you can get distance and then, calculate how many liters would be needed. Show the value with three decimal places after the point.
 // The input file contains two integers. The first one is the spent time in the trip (in hours). The second one is the average speed during the trip (in Km/h).
 
 #include<iostream>
@@ -391,7 +391,7 @@ int main(){
     return 0;
 }
 
-// Problem 18 - In this problem you have to read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed. The possible banknotes are 100, 50, 20, 10, 5, 2 and 1. Print the read value and the list of banknotes.
+// Problem 1018 - In this problem you have to read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed. The possible banknotes are 100, 50, 20, 10, 5, 2 and 1. Print the read value and the list of banknotes.
 // The input file contains an integer value N (0 < N < 1000000).
 
 #include<iostream>
@@ -430,7 +430,7 @@ int main(){
     return 0;
 }
 
-// Problem 19 - Read an integer value, which is the duration in seconds of a certain event in a factory, and inform it expressed in hours:minutes:seconds.
+// Problem 1019 - Read an integer value, which is the duration in seconds of a certain event in a factory, and inform it expressed in hours:minutes:seconds.
 // The input file contains an integer N.
 
 #include<iostream>
@@ -449,7 +449,7 @@ int main(){
     return 0;
 }
 
-// Problem 20 - Read an integer value corresponding to a person's age (in days) and print it in years, months and days, followed by its respective message “ano(s)”, “mes(es)”, “dia(s)”.
+// Problem 1020 - Read an integer value corresponding to a person's age (in days) and print it in years, months and days, followed by its respective message “ano(s)”, “mes(es)”, “dia(s)”.
 // Note: only to facilitate the calculation, consider the whole year with 365 days and 30 days every month. In the cases of test there will never be a situation that allows 12 months and some days, like 360, 363 or 364. This is just an exercise for the purpose of testing simple mathematical reasoning.
 // The input file contains 1 integer value.
 
@@ -472,7 +472,7 @@ int main(){
     return 0;
 }
 
-// Problem 21 - Read a value of floating point with two decimal places. This represents a monetary value. After this, calculate the smallest possible number of notes and coins on which the value can be decomposed. The considered notes are of 100, 50, 20, 10, 5, 2. The possible coins are of 1, 0.50, 0.25, 0.10, 0.05 and 0.01. Print the message “NOTAS:” followed by the list of notes and the message “MOEDAS:” followed by the list of coins.
+// Problem 1021 - Read a value of floating point with two decimal places. This represents a monetary value. After this, calculate the smallest possible number of notes and coins on which the value can be decomposed. The considered notes are of 100, 50, 20, 10, 5, 2. The possible coins are of 1, 0.50, 0.25, 0.10, 0.05 and 0.01. Print the message “NOTAS:” followed by the list of notes and the message “MOEDAS:” followed by the list of coins.
 // The input file contains a value of floating point N (0 ≤ N ≤ 1000000.00).
 
 #include<iostream>
@@ -542,3 +542,91 @@ int main() {
 
     return 0;
 }
+
+// Problem 1035 - Read 4 integer values A, B, C and D. Then if B is greater than C and D is greater than A and if the sum of C and D is greater than the sum of A and B and if C and D were positives values and if A is even, write the message “Valores aceitos” (Accepted values). Otherwise, write the message “Valores nao aceitos” (Values not accepted).
+// Four integer numbers A, B, C and D.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int A,B,C,D;
+    cin>>A>>B>>C>>D;
+
+    int sumAB = A+B;
+    int sumCD = C+D;
+
+    if(B>C && D>A && sumCD>sumAB && C>0 && D>0 && A%2==0){
+        cout<<"Valores aceitos"<<endl;
+    }
+    else{
+        cout<<"Valores nao aceitos"<<endl;
+    }
+
+    return 0;
+}
+
+// Problem 1036 - Read 3 floating-point numbers. After, print the roots of bhaskara’s formula. If it's impossible to calculate the roots because a division by zero or a square root of a negative number, presents the message “Impossivel calcular”.
+// Read 3 floating-point numbers (double) A, B and C.
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    double A,B,C;
+    cin>>A>>B>>C;
+
+    double delta = (B*B)-(4*A*C);
+    if(A==0 || delta<0){
+        cout<<"Impossivel calcular"<<endl;
+    }
+    else{
+        double R1=((-B)+sqrt(delta))/(2*A);
+        double R2=((-B)-sqrt(delta))/(2*A);
+        cout<<fixed<<setprecision(5);
+        cout<<"R1 = "<<R1<<endl;
+        cout<<"R2 = "<<R2<<endl;
+    }
+
+    return 0;
+}
+
+// Problem 1037 - You must make a program that read a float-point number and print a message saying in which of following intervals the number belongs: [0,25] (25,50], (50,75], (75,100]. If the read number is less than zero or greather than 100, the program must print the message “Fora de intervalo” that means "Out of Interval".
+// The symbol '(' represents greather than. For example:
+// [0,25] indicates numbers between 0 and 25.0000, including both.
+// (25,50] indicates numbers greather than 25 (25.00001) up to 50.0000000.
+// The input file contains a floating-point number.
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    float n;
+    cin>>n;
+
+    if(n>=0&&n<=25){
+        cout<<"Intervalo [0,25]"<<endl;
+    }
+    else if(n>25&&n<=50){
+        cout<<"Intervalo (25,50]"<<endl;
+    }
+    else if(n>50&&n<=75){
+        cout<<"Intervalo (50,75]"<<endl;
+    }
+    else if(n>75&&n<=100){
+        cout<<"Intervalo (75,100]"<<endl;
+    }
+    else{
+        cout<<"Fora de intervalo"<<endl;
+    }
+
+    return 0;
+}
+
